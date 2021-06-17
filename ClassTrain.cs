ffdba54ab5c413340e331be9eeb66fe1938f1e49
YuryMishurny <60823265+YuryMishurny@users.noreply.github.com>
2021-06-17 19:45:20 +0300

    initial commit

---
+++ ClassTrain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Train
+    {
+        private string _direction;
+        private Random _random;
+        private int _trainNumber;
+        private List<int> _railCars = new List<int>();
+
+        public int NumberTickets { get; private set; }
+
+        public Train()
+        {
+            _random = new Random();
+            _trainNumber = _random.Next(100, 1000);
+        }
+
+        public int GetNumberRailCars()
+        {
+            return _railCars.Count();
+        }
+
+        public void CreateRailCars()
+        {
+            while (_railCars.Sum() < NumberTickets)
+            {
+                _railCars.Add(_random.Next(10, 20));
+            }
+
+            Console.WriteLine("Сформировано " + _railCars.Count() + " вагонов");
+        }
+
+        public void CreateDirection()
+        {
+            Console.WriteLine("Введите направление поезда (пункт А - пункт Б):");
+            _direction = Console.ReadLine();
+            Console.WriteLine("Направление " + _direction + " сформировано.");
+        }
+
+        public void SellTickets()
+        {
+            NumberTickets = _random.Next(50, 150);
+            Console.WriteLine(NumberTickets + " билетов продано.");
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Поезд № - " + _trainNumber + ". Направление - " + _direction + ". Количество пассажиров  - " + NumberTickets);
+
+            for (int i = 0; i < _railCars.Count; i++)
+            {
+                Console.WriteLine("Вагон номер " + (i + 1) + " Вместительность " + _railCars[i] + " пассажиров");
+            }
+
+            if (_railCars.Count() > 0)
+                Console.WriteLine($"Занято {NumberTickets} мест из {_railCars.Sum()} доступных");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Train train = new Train();
+            Queue<Train> trains = new Queue<Train>();
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                if (trains.Count > 0)
+                    trains.Peek().ShowInfo();
+                else
+                    Console.WriteLine("Рейс отсувствует");
+
+                Console.WriteLine("\nСоставление плана поезда\n1 - Создать направление\n2 - Продать билеты\n3 - Сформировать поезд\n" +
+                    "4 - Отправить поезд\n5 - Exit");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        if (trains.Count <= 0)
+                        {
+                            train.CreateDirection();
+                            trains.Enqueue(train);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Направление уже создано");
+                        }
+                        break;
+                    case "2":
+                        if (trains.Count > 0)
+                        {
+                            if (train.NumberTickets == 0)
+                                train.SellTickets();
+                            else
+                                Console.WriteLine("Все билеты проданы");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Направление не создано, невозможно осуществить продажу билетов");
+                        }
+                        break;
+                    case "3":
+                        if (train.NumberTickets > 0)
+                        {
+                            if (train.GetNumberRailCars() <= 0)
+                                train.CreateRailCars();
+                            else
+                                Console.WriteLine("Поезд уже сформирован");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Для начала необходимо создать направление и продать билеты");
+                        }
+                        break;
+                    case "4":
+                        if (train.GetNumberRailCars() > 0)
+                        {
+                            trains.Dequeue();
+                            Console.WriteLine("Поезд ушел");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Поезд не готов к отправке");
+                        }
+                        break;
+                    case "5":
+                        isOpen = false;
+                        break;
+                    default:
+                        Console.WriteLine("Такой команды не существует...");
+                        break;
+                }
+
+                Console.WriteLine("Нажмите любую клавишу для продолжения...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
